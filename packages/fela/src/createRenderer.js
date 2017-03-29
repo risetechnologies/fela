69515e8fc053721e657ec7b6c9a78495fb384d8e
menelike <mitschnacker@gmx.net>
2017-03-29 17:39:55 +0200

    fix merge conflicts in conjunction with media and pseudo rules

--- packages/fela/src/createRenderer.js
+++ packages/fela/src/createRenderer.js
@@ -248,7 +248,7 @@ export default function createRenderer(
               )
 
             renderer.cache[declarationReference] = className
-            renderer.propCache[className] = property
+            if (!pseudo && !media) renderer.propCache[className] = property
 
             const cssDeclaration = cssifyDeclaration(property, value)
             const selector = generateCSSSelector(className, pseudo)
