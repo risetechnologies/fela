c23c680bc7d2fb6ef326e91cc0fa188efcc551d0
menelike <mitschnacker@gmx.net>
2017-03-29 17:28:16 +0200

    fix merge conflicts in conjunction with media and pseudo rules

--- modules/createRenderer.js
+++ modules/createRenderer.js
@@ -249,7 +249,7 @@ export default function createRenderer(
               generateClassName(++renderer.uniqueRuleIdentifier)
 
             renderer.cache[declarationReference] = className
-            renderer.propCache[className] = property
+            renderer.propCache[className] = media + pseudo + property
 
             const cssDeclaration = cssifyDeclaration(property, value)
             const selector = generateCSSSelector(className, pseudo)
