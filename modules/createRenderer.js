d645bfe55883e87cf5a0c54f99b2f426b0877c7e
menelike <mitschnacker@gmx.net>
2017-03-29 17:38:18 +0200

    Revert "fix merge conflicts in conjunction with media and pseudo rules"
    This reverts commit c23c680bc7d2fb6ef326e91cc0fa188efcc551d0.

--- modules/createRenderer.js
+++ modules/createRenderer.js
@@ -249,7 +249,7 @@ export default function createRenderer(
               generateClassName(++renderer.uniqueRuleIdentifier)
 
             renderer.cache[declarationReference] = className
-            renderer.propCache[className] = media + pseudo + property
+            renderer.propCache[className] = property
 
             const cssDeclaration = cssifyDeclaration(property, value)
             const selector = generateCSSSelector(className, pseudo)
