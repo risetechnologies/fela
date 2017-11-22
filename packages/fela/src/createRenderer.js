1ac05fe20e7ed3fced92fff1e10f21cf502eb101
menelike <mitschnacker@gmx.net>
2017-11-22 07:57:15 +0100

    add propCache

--- packages/fela/src/createRenderer.js
+++ packages/fela/src/createRenderer.js
@@ -67,6 +67,7 @@ export default function createRenderer(
     // use a flat cache object with pure string references
     // to achieve maximal lookup performance and memoization speed
     cache: {},
+    propCache: {},
 
     getNextRuleIdentifier() {
       return ++renderer.uniqueRuleIdentifier
@@ -176,6 +177,7 @@ export default function createRenderer(
       renderer.uniqueRuleIdentifier = 0
       renderer.uniqueKeyframeIdentifier = 0
       renderer.cache = {}
+      renderer.propCache = {}
 
       renderer._emitChange({
         type: CLEAR_TYPE,
@@ -274,6 +276,7 @@ Check http://fela.js.org/docs/basics/Rules.html#styleobject for more information
               support,
             }
 
+            if (!pseudo && !media) renderer.propCache[className] = property
             renderer.cache[declarationReference] = change
             renderer._emitChange(change)
           }
